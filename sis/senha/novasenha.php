3ecc39b14fbb72a5415c80223a1c765b853bb691
Walter Junior <93939362+WalteJunior@users.noreply.github.com>
2024-11-09 15:11:01 -0300

    Projeto

--- sis/senha/novasenha.php
+++ sis/senha/novasenha.php
@@ -1,16 +1,17 @@
 <head>
-    <link rel="stylesheet" href="\sisescala\css\login.css">
+    <link rel="stylesheet" href="../../css/login.css">
     <title>Recuperar Senha</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <form action="verificar.php" method="post" class="login-form">
-    <h1>Recuperação de Senha</h1>
+    <h1 style="color: black;">Recuperação de Senha</h1>
 
     <div class="form-input-material">
         <input type="text" name="usuario" id="username" placeholder=" " maxlength="25" autocomplete="off" class="form-control-material" required />
         <label for="username">Informe Username</label>
     </div>
 
-    <button type="submit" class="btn btn-primary btn-ghost">Verificar Usuario</button><br>
+    <button type="submit" class="btn btn-primary btn-ghost" style="margin-bottom: 5px;">Verificar Usuario</button>
     <a href="../../logout.php"><button type="button" class="btn btn-secondary btn-ghost">Voltar</button></a>
 
 </form>
--- sis/senha/redefinir.php
+++ sis/senha/redefinir.php
@@ -11,11 +11,12 @@ $usuario = htmlspecialchars($_GET['usuario']);
 ?>
 
 <head>
-    <link rel="stylesheet" href="\sisescala\css\login.css">
+    <link rel="stylesheet" href="../../css/login.css">
     <title>Nova senha</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <form action="atualiza_senha.php" method="post" class="login-form">
-    <h1>Atualizar Senha para: <?php echo $usuario; ?></h1>
+    <h1 style="color: black;">Atualizar Senha para: <?php echo $usuario; ?></h1>
 
     <div class="form-input-material">
         <input type="hidden" name="usuario" value="<?php echo $usuario; ?>" />
